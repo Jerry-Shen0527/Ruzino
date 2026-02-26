7be7e30eedb459783590c7da5f1e8b550be10d19
Pengfei Shen <jerry_shen0527@outlook.com>
2026-02-27 00:07:11 +0800

    WIP

--- source/Runtime/usdview_widget/tests/usdview_widget.cpp
+++ source/Runtime/usdview_widget/tests/usdview_widget.cpp
@@ -4,17 +4,42 @@
 #include <spdlog/spdlog.h>
 
 #include "GUI/window.h"
+#include "pxr/imaging/garch/glApi.h"
 #include "pxr/usd/usd/stage.h"
 #include "pxr/usd/usdGeom/sphere.h"
+#include "pxr/usdImaging/usdImagingGL/engine.h"
 #include "stage/stage.hpp"
 #include "widgets/usdtree/usd_fileviewer.h"
 
 using namespace Ruzino;
 
+// Graphics context initialization
+inline void CreateGLContext()
+{
+#ifdef _WIN32
+    HDC hdc = GetDC(GetConsoleWindow());
+    PIXELFORMATDESCRIPTOR pfd = {};
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+
+    int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+    SetPixelFormat(hdc, pixelFormat, &pfd);
+
+    HGLRC hglrc = wglCreateContext(hdc);
+    wglMakeCurrent(hdc, hglrc);
+#endif
+}
+
 int main()
 {
     spdlog::set_level(spdlog::level::debug);
     spdlog::set_pattern("%^[%T] %n: %v%$");
+
+    CreateGLContext();
+    GarchGLApiLoad();
 
     auto stage = create_global_stage("../../Assets/test_scene.usd");
     stage->create_sphere(pxr::SdfPath("/sphere"));
